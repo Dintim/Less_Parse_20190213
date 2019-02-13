a63239453403383cd62fd9ec1edd82d519b16d4c
Динара Мукашева <@local.itstep>
2019-02-13 12:07:27 +0600

    Add project files.

---
+++ ConsoleApp3/Model/Data.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Model
+{
+    public class Users
+    {
+        public List<results> results = new List<results>();
+    }
+
+    public class results
+    {
+        public string gender { get; set; }
+        public name name { get; set; }
+        public location location { get; set; }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(name.title + " " + name.first + " "+name.last);
+            Console.WriteLine("Адрес проживания:");
+            Console.WriteLine("{0}, {1} ({2})", location.postcode, location.city, location.street);
+        }
+
+    }
+
+    public class name
+    {
+        public string title { get; set; }
+        public string first { get; set; }
+        public string last { get; set; }
+    }
+
+    public class location
+    {
+        public string street { get; set; }
+        public string city { get; set; }
+        public string postcode { get; set; }
+        public coordinates coordinates { get; set; }
+    }
+
+    public class coordinates
+    {
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApp3.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string URL = "https://randomuser.me/api/?results=14";
+            ServiceUser serviceUser = new ServiceUser(URL);
+            Users users= serviceUser.GetUsers();
+            for(int i=0; i<users.results.Count; i++)
+            {
+                //results user = users.results[i];
+                users.results[i].PrintInfo();
+                Console.WriteLine("---------------------------------");
+            }
+        } 
+
+
+    }
+
+    
+}
